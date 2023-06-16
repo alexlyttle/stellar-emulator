c253192f8e5f28f8841bc59171e924517a5324da
Alex Lyttle <a.j.lyttle@outlook.com>
2023-06-16 17:38:02 +0100

    Add cleaned version of grid

--- scripts/yaguangli2023_grid.py
+++ scripts/yaguangli2023_grid.py
@@ -3,12 +3,14 @@ import numpy as np
 import pandas as pd
 
 PATH = "/mnt/data-storage/yaguangli2023/stellar-models/grid_models_surface_effect_uncorrected"
+FILENAME = "grid_clean.h5"
+CLEAN = True
 TRACK_COLS = [
-    "star_mass", 
-    "Yinit", 
-    "Zinit", 
-    "amlt", 
-    "fov_core", 
+    "star_mass",
+    "Yinit",
+    "Zinit",
+    "amlt",
+    "fov_core",
     "fov_shell"
 ]
 STAR_COLS = [
@@ -20,12 +22,19 @@ STAR_COLS = [
     'log_Lnuc',
     'Dnu_freq_o',
     'eps_o',
+    'delta_Pg'
 ]
 MODE_COLS = [
     'mode_freq_o'
 ]
+CONST = {
+    "msun": 1.9884098706980504e33,
+    "rsun": 6.957e10,
+    "lsun": 3.8280000000000003e33,
+    "Tsun": 5.7720034291e3,
+}
 
-output_filename = os.path.join(PATH, "grid.h5")
+output_filename = os.path.join(PATH, FILENAME)
 input_filenames = [os.path.join(PATH, name) for name in os.listdir(PATH) if name.endswith(".npy")]
 
 def load_tracks(filename):
@@ -54,12 +63,26 @@ def tracks_to_dataframe(tracks):
         df[key] = np.concatenate(tracks[key])
     return add_index_cols(df)
 
+def clean_dataframe(df):
+    """Remove pre main sequence points."""
+    print("Removing pre-main sequence points")
+    df["f_nuc"] = 10**df.log_Lnuc / df.luminosity
+    df["delta_X"] = df.Xinit - df.center_h1
+    idxs = []
+    for _, group in df.groupby("track"):
+        hburn = (group["f_nuc"] > 0.999) & (group["delta_X"] > 0.0015)
+        mask = group.index < hburn.idxmax()
+        idxs.append(group.index[mask].to_numpy())
+    return df.drop(index=np.concatenate(idxs))
+
 def append_hdf(filename, df, group, index_cols, data_cols):
     df.drop_duplicates(index_cols).set_index(index_cols)[data_cols].to_hdf(filename, group, append=True, format="table")
 
 def update(input_filename):
     print("Loading", input_filename)
     df = tracks_to_dataframe(load_tracks(input_filename))
+    if CLEAN:
+        df = clean_dataframe(df)
     
     print("Appending tracks to", output_filename)
     append_hdf(output_filename, df, "tracks", "track", TRACK_COLS)
@@ -70,9 +93,14 @@ def update(input_filename):
     print("Appending modes to", output_filename)
     append_hdf(output_filename, df, "modes", ["track", "star", "n", "l"], MODE_COLS)
 
+def add_metadata(filename):
+    print("Adding metadata")
+    pd.Series(CONST).to_hdf(filename, "constants", format="table")
+
 def main():
     if os.path.exists(output_filename):
         raise ValueError("File", output_filename, "exists. Delete or try a different file name.")
+    add_metadata(output_filename)
     for input_filename in input_filenames:
         update(input_filename)
 
