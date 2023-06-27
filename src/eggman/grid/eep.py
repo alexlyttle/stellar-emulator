ae1cefcf0d4212aa4172c99da05514c3a59d2b79
Alex Lyttle <a.j.lyttle@outlook.com>
2023-06-27 17:39:55 +0100

    Add secondary EEP to function

--- src/eggman/grid/eep.py
+++ src/eggman/grid/eep.py
@@ -1,20 +1,45 @@
+import numpy as np
 import pandas as pd
 
 from .defaults import *
 from .star import nuclear_luminosity_fraction, delta_hydrogen, log_surface_gravity
 
-def calculate_eep(grid):
+def calculate_eep(grid, keys, alpha=0.2, weights=None, scale=None):
     """Calculate the primary and secondary equivalent evolutionary phase for
     tracks in the grid. This groups the grid by track and assumes that each
     track is in chronological order.
     
+    d[i] = d[i-1] + sum_over_j(weights[j] * (x[j][i] - x[j][i-1])**2)**alpha
+    
+    Args:
+        grid (pandas.DataFrame): 
+            Must contain MASS, RAD, YINI, ZINI, XCEN and all of `keys`.
+        keys (list of str):
+            Keys from which to calculate secondary EEP distance metric.
+        alpha (float): 
+            Exponent of distance metric, defaults to 0.2 (Li et al. 2022).            
+        weights (list of float):
+            Weights to apply to each key for distance metric, defaults to equal
+            weighting.
+        scale (list of float):
+            Scale factor for distance metric across each primary EEP. Defaults
+            to 1.0 for each phase.
     Returns:
         primary (pandas.Series): Primary EEP (0 = ZAMS, 1 = IAMS, 2 = TAMS, 
             3 = MAXNB, 4 = END) and -1 means unnassigned phase.
         secondary (pandas.Series): Not yet implemented.
     """
+    if weights is None:
+        weights = np.ones(len(keys))
+    
+    if scale is None:
+        scale = 4 * [1]
+        
+    # Location of each primary EEP phase
+    loc = [sum(scale[:i]) for i in range(len(scale))]
+
     primary = pd.Series(-1, grid.index)
-    secondary = None
+    secondary = pd.Series(np.nan, grid.index)
     grid["delta_X"] = delta_hydrogen(grid)
     grid["f_nuc"] = nuclear_luminosity_fraction(grid)
     grid["log_g"] = log_surface_gravity(grid)
@@ -37,9 +62,21 @@ def calculate_eep(grid):
 
         bounds = [zams, iams, tams, maxn, end]
 
-        phase = [group.index[(group.index >= low) & (group.index < up)] for low, up in zip(bounds[:-1], bounds[1:])]
+        phase = [
+            group.index[
+                (group.index >= low) & (group.index < up)
+            ] for low, up in zip(bounds[:-1], bounds[1:])
+        ]
+        distance = ((weights*group[keys].diff()**2).sum(axis=1)**alpha).cumsum()
+        
+        for i, p in enumerate(phase):
+            primary.loc[p] = i
 
-        for i, phi in enumerate(phase):
-            primary.loc[phi] = i
+            lower = bounds[i]
+            upper = bounds[i+1]
+            secondary.loc[p] = (
+                loc[i] + scale[i] * (distance.loc[p] - distance.loc[lower])
+                / (distance.loc[upper] - distance.loc[lower])
+            )
 
     return primary, secondary
