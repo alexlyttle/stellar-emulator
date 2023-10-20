9a70921f1f26e154a69ad1aa00c2411ac25f8942
Alex Lyttle <a.j.lyttle@outlook.com>
2023-10-20 17:21:18 +0100

    Add nested sampling example notebook

--- src/eggman/emulator.py
+++ src/eggman/emulator.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 import jax.numpy as jnp
-# from pytensor import tensor as pt
+from pytensor import tensor as pt
 from jax.nn import elu as elu_jax
 from tensorflow.keras.models import load_model
 
@@ -10,8 +10,8 @@ from . import PACKAGEDIR
 def elu(x, alpha=1.0):
 	return np.where(x >= 0, x, alpha*(np.exp(x) - 1))
 
-# def elu_pt(x, alpha=1.0):
-#     return pt.where(x >= 0, x, alpha*(pt.exp(x) - 1))
+def elu_pt(x, alpha=1.0):
+    return pt.where(x >= 0, x, alpha*(pt.exp(x) - 1))
 
 
 class Emulator:
@@ -58,21 +58,21 @@ class Emulator:
         x = jnp.matmul(x, self.weights[-2]) + self.weights[-1]
         return self.offset + self.scale * x
 
-    # def model_pt(self, x):
-    #     """Emulator model.
+    def model_pt(self, x):
+        """Emulator model.
         
-    #     Args:
-    #         x (array-like): Neural network inputs.
+        Args:
+            x (array-like): Neural network inputs.
         
-    #     Returns:
-    #         jax.numpy.ndarray: Neural network outputs.
-    #     """
-    #     x = x - self.weights[0]
-    #     x /= self.weights[1]**0.5
-    #     for w, b in zip(self.weights[3:-2:2], self.weights[4:-1:2]):
-    #         x = elu_pt(pt.matmul(x, w) + b)
-    #     x = pt.matmul(x, self.weights[-2]) + self.weights[-1]
-    #     return self.offset + self.scale * x
+        Returns:
+            jax.numpy.ndarray: Neural network outputs.
+        """
+        x = x - self.weights[0]
+        x /= self.weights[1]**0.5
+        for w, b in zip(self.weights[3:-2:2], self.weights[4:-1:2]):
+            x = elu_pt(pt.matmul(x, w) + b)
+        x = pt.matmul(x, self.weights[-2]) + self.weights[-1]
+        return self.offset + self.scale * x
 
     def __call__(self, x, backend="jax"):
         """Emulator model.
@@ -87,8 +87,8 @@ class Emulator:
             return self.model_jax(x)
         elif backend == "numpy":
             return self.model(x)
-        # elif backend == "pytensor":
-        #     return self.model_pt(x)
+        elif backend == "pytensor":
+            return self.model_pt(x)
         elif backend == "tensorflow":
             return self.model_tf(x)
         else:
